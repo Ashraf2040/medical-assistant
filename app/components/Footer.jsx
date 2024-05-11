e02a2620fd82660aaeb964a862022e02d911d3c4
Ashraf2040 <m_a199025@yahoo.com>
2024-05-11 22:36:10 +0300

    mod2

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -5,18 +5,20 @@ import React from "react";
 export default function Footer() {
   return (
     <div
-      className="w-full  bg-gray-100 pb-4 pt-2 mt-10  gap-2 border-t-2  border-[#02B1BF]  flex items-center justify-between flex-col  px-4 h-fit "
+      className="w-full  bg-gray-100 pb-4 pt-4 mt-10  gap-4 border-t-2  border-[#02B1BF]  flex items-center justify-between flex-col  px-0.5 h-fit "
     >
-      <div className="flex w-[90%] justify-between  font-semibold text-[#02B1BF]">
+      <div className="flex justify-between px-4 gap-2 items-center w-full ">
+      <div className=" "><Image src={"/Logo-short.png"} alt="diaf" width={180} height={180} className="h-15 w-20"/>
+    
+    </div>
+      <div className="flex  self-end items-center gap-4 text-[12px] font-semibold text-[#02B1BF]">
         <Link href="/about">About</Link>
         <Link href="/privacy">Privacy Policy</Link>
         <Link href="/terms">Terms of Use</Link>
       </div>
 
-    <div className="flex justify-between px-2 items-center w-full"><Image src={"/Logo-short.png"} alt="diaf" width={180} height={180} className="h-15 w-20"/>
-    <p className="text-[#02B1BF] self-end">All Rights Reserved.©️ {(new Date().getFullYear())}</p>
-    </div>
-    
+  </div>
+    <p className="text-[#02B1BF] text-[14px]">All Rights Reserved.©️ {(new Date().getFullYear())}</p>
     </div>
   );
 }
