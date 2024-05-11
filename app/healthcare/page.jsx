e23034b9f2c8698c654b298d1ee39ff4d52e7c5a
Ashraf2040 <m_a199025@yahoo.com>
2024-05-11 15:41:05 +0300

    mod2

--- app/healthcare/page.jsx
+++ app/healthcare/page.jsx
@@ -22,13 +22,13 @@ export default function HealthCare() {
   console.log(currentSwiper)
   return (
    
-    <div className=' my-8 flex flex-col w-full items-center justify-center  '>
+    <div className=' my-8 px-2 gap-4 flex flex-col w-full items-center justify-center   '>
 
     
 
       {dataCards.map((item, index) => (
-        <div key={item.topic} className=' flex flex-col w-full items-center justify-center'> 
-          <h1 className='text-lg  mx-4   rounded-md  text-[#02B1BF]  my-6  p-1 px-2 text-center font-bold'>
+        <div key={item.topic} className='  flex flex-col w-full items-center justify-center shadow-xl rounded-md pb-4'> 
+          <h1 className='text-lg  mx-2     text-[#085f63]  my-6 b px-3   p-2 text-center font-bold'>
           {item.topic}
         </h1>
         
@@ -37,26 +37,26 @@ export default function HealthCare() {
         grabCursor={true}
         modules={[EffectCards]}
         onClick={() => {setCurrentSwiper(index)}}
-        className="mySwiper relative rounded-[18px] shadow-[0_0_10px_#ddd] w-full " 
+        className="mySwiper relative rounded-[18px]  w-full " 
       >
         
         {item.sources.map((value, index) => (
 
          
-          <SwiperSlide key={index} className='flex justify-center items-center w-full '>
+          <SwiperSlide key={index} className='flex justify-center items-center w-full  ring-[3px] ring-neutral-200 '>
             <Link href={`/healthcare/${value.title.replace(/\s+/g,'-') +currentSwiper}`} className='w-full rounded-[18px]'>
             <Image
       src={value.src}
       alt={value.title}
       width={800}
-      height={600}
-      className=" h-full rounded-[18px] w-full"
+      height={800}
+      className=" h-full w-full"
       
       priority
       key={1}
     />
             <span className='text-[#02B1BF] bg-white rounded-b-lg absolute top-0 right-0 px-4 text-[18px] py-1'>{index+1} of {item.sources.length}</span>
-            <p className="  text-center w-full py-4 text-[18px]  absolute bottom-0  text-[#02B1BF]  font-bold  bg-white  ">{value.title}</p>
+            <p className="  text-center w-full py-4 text-[18px]  absolute bottom-0  text-[#085f63]   font-bold  bg-white z-20 ">{value.title}</p>
             </Link>
             
           </SwiperSlide>) )}
