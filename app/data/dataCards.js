6282ce730fd2a519fd1a4a4f3b4b24cc92f7409b
Ashraf2040 <m_a199025@yahoo.com>
2024-05-11 10:10:38 +0300

    cardss

---
+++ app/data/dataCards.js
@@ -0,0 +1,160 @@
+ const dataCards = [  
+
+    {
+      topic:"General Health and Safety During Hajj",
+  
+    sources :[
+      { 
+        src :"/1.jpg",
+        title: "Health Tips for Pilgrims",
+        cards:["/health-tips/1card.jpg","/health-tips/2card.jpg","/health-tips/3card.jpg","/health-tips/4card.jpg","/health-tips/5card.jpg","/health-tips/6card.jpg","/health-tips/7card.jpg","/health-tips/8card.jpg","/health-tips/9card.jpg","/health-tips/10card.jpg","/health-tips/11card.jpg","/health-tips/12card.jpg","/health-tips/13card.jpg","/health-tips/14card.jpg","/health-tips/15card.jpg","/health-tips/16card.jpg","/health-tips/17card.jpg"]
+  
+       },
+      { 
+        src :"/2.jpg",
+        title: "Health Tips for using FaceMasks",
+       },
+      { 
+        src :"/3.jpg",
+        title: "Physical Fitness Preparation for Hajj",
+       },
+      { 
+        src :"/4.jpg",
+        title: "Managing Heat and Hydration during Hajj",
+       },
+     
+    ]},
+    {
+      topic:"Environmental Safety During Hajj",
+  
+    sources :[
+      { 
+        src :"/1.jpg",
+        title: " Environment in Islam",
+       },
+      { 
+        src :"/2.jpg",
+        title: "Environmental Safety During Hajj ",
+       },
+      { 
+        src :"/3.jpg",
+        title: " Housing and Sustenance Environment",
+       },
+      { 
+        src :"/3.jpg",
+        title: "Environment of Transportation and Mobility",
+       },
+      { 
+        src :"/3.jpg",
+        title: "Environment of the Haram and the Sacred Sites",
+       },
+      { 
+        src :"/3.jpg",
+        title: "Energy and Environment",
+       },
+      { 
+        src :"/3.jpg",
+        title: "Environmental Emergencies",
+       },
+      { 
+        src :"/3.jpg",
+        title: "Preventing Respiratory Infections During Hajj",
+       },
+      { 
+        src :"/3.jpg",
+        title: "Maintaining Digestive Health During Hajj ",
+       },
+     
+    ]},
+    {
+      topic:"Injury Prevention and Management ",
+  
+    sources :[
+      { 
+        src :"/1.jpg",
+        title: "Maintaining Skin Health and Preventing Abrasions During Hajj",
+       },
+      { 
+        src :"/2.jpg",
+        title: "Safe Shaving Practices During Hajj",
+       },
+      { 
+        src :"/3.jpg",
+        title: "Avoiding Muscle Stress During Hajj",
+       },
+      { 
+        src :"/3.jpg",
+        title: "Managing Bruises During Hajj ",
+       },
+      { 
+        src :"/3.jpg",
+        title: "Managing Fractures During Hajj",
+       },
+      { 
+        src :"/3.jpg",
+        title: "Managing Wounds During Hajj",
+       },
+      { 
+        src :"/3.jpg",
+        title: "Managing Nosebleeds During Hajj",
+       },
+      { 
+        src :"/3.jpg",
+        title: "Managing Hypoglycemia During Hajj",
+       },
+     
+     
+    ]},
+    {
+      topic:"Specific Health Conditions During Hajj",
+  
+    sources :[
+      { 
+        src :"/1.jpg",
+        title: "Diabetic Pilgrims Performing Hajj",
+       },
+      { 
+        src :"/2.jpg",
+        title: "Asthmatic Pilgrims Performing Hajj",
+       },
+      { 
+        src :"/3.jpg",
+        title: "Allergic Patients Performing Hajj",
+       },
+      { 
+        src :"/3.jpg",
+        title: "Kidney Patients Performing Hajj",
+       },
+      { 
+        src :"/3.jpg",
+        title: "Managing Epilepsy During Hajj",
+       },
+     
+    ]},
+    {
+      topic:"Women's Health During Hajj",
+  
+    sources :[
+      { 
+        src :"/1.jpg",
+        title: "Health Tips for Women",
+       },
+      { 
+        src :"/2.jpg",
+        title: "Health Tips for the Pregnant Women",
+       },
+      { 
+        src :"/3.jpg",
+        title: "When Pregnant Women Should Postpone Hajj?",
+       },
+      { 
+        src :"/3.jpg",
+        title: "Infants and Children during the Hajj",
+       },
+     
+    ]},
+  
+    
+   ]
+
+   export default dataCards
--- app/healthcare/[id]/page.jsx
+++ app/healthcare/[id]/page.jsx
@@ -4,7 +4,7 @@
 import React, { useRef, useState } from 'react';
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
- import { dataCards } from '../page';
+import dataCards from '../../data/dataCards'
 // Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/effect-creative';
